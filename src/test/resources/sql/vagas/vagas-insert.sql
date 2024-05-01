f6d1f17cda75281113c3a0474f622a0b74c70fcf
Juliana Ransani <juliana.ransani.2@gmail.com>
2024-05-01 14:34:05 -0300

    Recurso de Vagas parte2

---
+++ src/test/resources/sql/vagas/vagas-insert.sql
@@ -0,0 +1,8 @@
+insert into USUARIOS (id, username, password, role) values (100, 'ana@email.com', '$2a$12$gcTP1oLNOlOeE6NF6Hg9Ce2r4PKyFOC6xB2ni6L8/TBb.sTC/CBtK', 'ROLE_ADMIN');
+insert into USUARIOS (id, username, password, role) values (101, 'bia@email.com', '$2a$12$gcTP1oLNOlOeE6NF6Hg9Ce2r4PKyFOC6xB2ni6L8/TBb.sTC/CBtK', 'ROLE_CLIENTE');
+insert into USUARIOS (id, username, password, role) values (102, 'bob@email.com', '$2a$12$gcTP1oLNOlOeE6NF6Hg9Ce2r4PKyFOC6xB2ni6L8/TBb.sTC/CBtK', 'ROLE_CLIENTE');
+
+insert into VAGAS (id, codigo, status) values (10, 'A-01', 'LIVRE');
+insert into VAGAS (id, codigo, status) values (20, 'A-02', 'LIVRE');
+insert into VAGAS (id, codigo, status) values (30, 'A-03', 'OCUPADA');
+insert into VAGAS (id, codigo, status) values (40, 'A-04', 'LIVRE');
